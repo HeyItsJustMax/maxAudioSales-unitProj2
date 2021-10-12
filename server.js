ddd03bf6fdc83a13701242725b6ad8d4e8ac87c9
Maximilian Rice <maximilian.rice1@gmail.com>
2021-10-12 19:54:15 -0400

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config() // load ENV variables
 const express = require("express") // import express
 const morgan = require("morgan") // import morgan
 const methodOverride = require("method-override")
+const mongoose = require("mongoose")
 const GearRouter = require("./controllers/gear")
 const UserRouter = require("./controllers/user")
 const session = require("express-session")
@@ -19,6 +20,31 @@ const MongoStore = require("connect-mongo")
 
 const app = express()
 
+////////////////////////////////////
+//
+////////////////////////////////////
+
+const db = mongoose.connection
+
+
+
+////////////////////////////////////
+// DATABASE
+////////////////////////////////////
+
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Error / Success
+db.on("error", (err) => console.log(err.message + " is mongod not running?"))
+db.on("connected", () => console.log("mongod connected: ", MONGODB_URI))
+db.on("disconnected", () => console.log("mongod disconnected"))
+
 
 
 /////////////////////////////////////
@@ -26,9 +52,10 @@ const app = express()
 /////////////////////////////////////
 
 app.use(morgan("tiny")) // logging
-app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(methodOverride("_method")) // override for post, put, and delete requests from forms
 app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
 app.use(express.static("public")) // serve files from public statically
+app.use(express.json()) // returns middleware that only parses JSON - may not need it depending on your project
 app.use(session({
     secret: process.env.SECRET,
     store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
